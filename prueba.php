f72d88b5c13d307df43334985a83b3129b8446cd
Carlos Barrero <barrerot@gmail.com>
2013-06-22 13:29:47 +0200

    prueba.php

--- prueba.php
+++ prueba.php
@@ -25,12 +25,6 @@ date_default_timezone_set('Europe/Madrid');
     <link rel="stylesheet" href="components/lungo/lungo.css">
     <link rel="stylesheet" href="components/lungo/theme.lungo.css">
 
- <!--   <script type="text/javascript">
-        function notificationNew(){
-            Lungo.Notification.html("<form class='margined'><label></label><label>Inserta la nueva toma</label><label class='select'><select class='custom'><option value='1'>Instanyl 200 microgramos</option><option value='2'>Paracetamol 1g VO</option><option value='3'>Nolotil VO</option></select></label></form>", "");
-        }
-    </script>
--->
 </head>
 
 <body class="app">

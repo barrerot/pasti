73046749e032964b78ebea2b6ad7c2c05787af65
Carlos Barrero <barrerot@gmail.com>
2013-06-23 23:01:19 +0200

    a

--- rest/addToma.php
+++ rest/addToma.php
@@ -8,11 +8,14 @@ include "../inc/class.pastilla.php";
 include "../inc/class.toma.php";
 
 date_default_timezone_set('Europe/Madrid');
+$verano = 7200;
 
 $pastillaid = $_GET['pid'];
 
+$hour = time() - $verano;
+
 $connection = Database::Connect();
-$query = "INSERT INTO  `pastillero`.`toma` (`tomaid`,`timestamp`,`pastillaid`) VALUES (NULL ,  '".time()."',  '".$pastillaid."');";
+$query = "INSERT INTO  `pastillero`.`toma` (`tomaid`,`timestamp`,`pastillaid`) VALUES (NULL ,  '".$hour."',  '".$pastillaid."');";
 $cursor = Database::Reader($query, $connection);
 echo $query;
 

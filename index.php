308281f71ac817ef4551f13804a2b4a46254f819
Carlos Barrero <barrerot@gmail.com>
2013-08-03 22:51:09 +0200

    d

--- index.php
+++ index.php
@@ -212,6 +212,7 @@ $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "
                                 template="<ul>{{#tomas}}\
                                              <li id='{{tomaid}}'>\
                                                 <div class='right'>\
+                                                    <a href='#' class='button small' data-theme='theme.red.css' onclick='edit_toma({{tomaid}})'>Editar</a>\
                                                     <a href='#' class='button small' data-theme='theme.red.css' onclick='delete_toma({{tomaid}})'>Borrar</a>\
                                                 </div>\
                                                 <strong>{{pastilla}}</strong>\

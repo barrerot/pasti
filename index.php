b77b63006eb45dadef87370f2f05a35a63c3b362
Carlos Barrero <barrerot@gmail.com>
2013-08-03 23:15:27 +0200

    d

--- index.php
+++ index.php
@@ -92,6 +92,15 @@ $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "
                         <option value="01">01</option>
                         <option value="02">02</option>
                         <option value="03">03</option>
+                        <option value="04">04</option>
+                        <option value="05">05</option>
+                        <option value="06">06</option>
+                        <option value="07">07</option>
+                        <option value="08">08</option>
+                        <option value="09">09</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
                     </select>
                 </label>
                 <label class="select">
@@ -100,6 +109,14 @@ $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "
                         <option value="05">05</option>
                         <option value="10">10</option>
                         <option value="15">15</option>
+                        <option value="20">20</option>
+                        <option value="25">25</option>
+                        <option value="30">30</option>
+                        <option value="35">35</option>
+                        <option value="40">40</option>
+                        <option value="45">45</option>
+                        <option value="50">50</option>
+                        <option value="55">55</option>
                     </select>
                 </label>
             </fieldset>
